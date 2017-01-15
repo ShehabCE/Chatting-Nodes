6a70e9dc9b22c8429c9d51fbc328892307da1ae8
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2017-01-15 02:30:25 +0200

    Displaying online chatters

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 
+var nicknames = [];
+
 server.listen(3000);
 
 app.get('/', function(req, res) 
@@ -11,8 +13,27 @@ app.get('/', function(req, res)
 });
 
 io.sockets.on('connection', function(socket){
+
+	socket.on('new user', function(data, callback) {
+		if(nicknames.indexOf(data) != -1) {
+			callback(false);
+		} else {
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			io.sockets.emit('usernames', nicknames);
+		}
+	});
+
 	socket.on('send message', function(data) {
 		io.sockets.emit('New Message', data);
+	});
+
+	socket.on('disconnect', function(data) {
+		if(!socket.nickname)
+			return;
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		io.sockets.emit('usernames', nicknames);
 	});
 
 });
